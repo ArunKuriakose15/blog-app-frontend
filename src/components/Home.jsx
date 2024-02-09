db4f540fa584e6683db2f06fa07f5b1e7f9d1d2a
ArunKuriakose15 <arunkp1515@gmail.com>
2024-02-10 00:55:16 +0530

    view all post

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import NavBar from './NavBar'
 
 const Home = () => {
+    const backgroundStyle = {
+        backgroundImage: 'url("https://images.pexels.com/photos/1591056/pexels-photo-1591056.jpeg)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      };
   return (
-    <div>
+    
+    <div className='home-background'>
         <NavBar/>
-
-        <h1>Blog App</h1>
     </div>
   )
 }
