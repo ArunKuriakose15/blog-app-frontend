f70a8ee35177dbd5873a1dc1a660396c08792c6e
ArunKuriakose15 <arunkp1515@gmail.com>
2024-02-10 00:20:38 +0530

    login and signup

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const Home = () => {
+  return (
+    <div>
+        <NavBar/>
+
+        <h1>Blog App</h1>
+    </div>
+  )
+}
+
+export default Home
